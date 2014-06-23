0e981ff853465c499b01fea0651735b98f4352ac
geoffreyhale <geoffreyhale@gmail.com>
2014-06-23 14:49:40 -0700

    improved but behaving strange

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var numRows = 0;
 var numCols = 0;
 var cells = [];
+var ticksPassed = 0;
 
 function makeTable(inumRows,inumCols) {
     numRows = inumRows;
@@ -19,53 +20,66 @@ function drawTable() {
             document.write("<tr>");
             for ( var j = 0; j < numCols; j++ ) {
                 document.write("<td id='cell" + ( i * numRows + j ) + "' onClick='toggleCell("+i+","+j+")'>" +
-                    cells[i*numRows+j] +
+                    numAdjacent(i, j) +
                     "</td>");
             }
             document.write("</tr>");
         }
         document.write("</table>");
     }
+    document.write("Tick #:<span id='ticksPassed'>" + ticksPassed + "</span>");
 }
 
+
+/*** USER ACTIONS ***/
 function toggleCell(i,j) {
     cells[i*numRows+j] = !cells[i*numRows+j];
-    document.getElementById("cell" + ( i * numRows + j )).innerHTML=cells[i*numRows+j];
+    document.getElementById("cell" + ( i * numRows + j )).innerHTML = numAdjacent(i, j);
+    cellColorUpdate(i,j);
 }
 
-var gameTickLength = window.setInterval(nextTime, 1000);
 
-function nextTime() {
+/*** GAME TIMER ***/
+var gameTickLength = window.setInterval(nextTick, 2000);
+function nextTick() {
+    ticksPassed++;
+    document.getElementById("ticksPassed").innerHTML=ticksPassed;
+    evolveCells();
     for ( var i = 0; i < numRows; i++ ) {
         for (var j = 0; j < numCols; j++) {
-            evolveCell(i,j);
+            cellColorUpdate(i,j);
         }
     }
 }
 
-function evolveCell(i,j) {
-    /*if ( cells[i*numRows+j+1] && cells[i*numRows+j-1] ||
-        cells[i*numRows+j+1] && cells[i*(numRows-1)+j] ||
-        cells[i*numRows+j+1] && cells[i*(numRows+1)+j] ||
-        cells[i*numRows+j-1] && cells[i*(numRows-1)+j] ||
-        cells[i*numRows+j-1] && cells[i*(numRows-1)+j] ||
-        cells[i*(numRows-1)+j] && cells[i*(numRows-1)+j] ) {
-        cells[i*numRows+j] = true;
-        document.getElementById("cell" + ( i * numRows + j )).style.background="#ffaaaa";
-    } else {
-        cells[i*numRows+j] = false;
-        document.getElementById("cell" + ( i * numRows + j )).style.background="#eeeeee";
-    }*/
-    if ( cells[i*numRows+j] > 0 ) {
-        cells[i*numRows+j]++;
-        document.getElementById("cell" + ( i * numRows + j )).innerHTML=cells[i*numRows+j];
-        document.getElementById("cell" + ( i * numRows + j )).style.background="#88ddff";
-    } else {
-        document.getElementById("cell" + ( i * numRows + j )).style.background="#eeeeee";
+function evolveCells() {
+    var tempCells = cells;
+    for ( var i = 0; i < numRows; i++ ) {
+        for (var j = 0; j < numCols; j++) {
+            if ( numAdjacent(i, j) > 0 ) {
+                tempCells[i * numRows + j] = true;
+            } else {
+                tempCells[i * numRows + j] = false;
+            }
+            document.getElementById("cell" + ( i * numRows + j )).innerHTML = numAdjacent(i, j);
+        }
     }
+    cells = tempCells;
 }
 
-/*function cellUpdate(i,j,value) {
-    cells[i*numRows+j] = value;
-    document.getElementById("cell" + ( i * numRows + j )).innerHTML = cells[i*numRows+j];
-}*/
+function numAdjacent(i,j) {
+    var num = 0;
+    if ( cells[(i-1)*numRows+j] ) { num++; }
+    if ( cells[(i+1)*numRows+j] ) { num++; }
+    if ( cells[(i)*numRows+j-1] ) { num++; }
+    if ( cells[(i)*numRows+j+1] ) { num++; }
+    return num;
+}
+
+function cellColorUpdate(i,j) {
+    if ( cells[i*numRows+j] == true ) {
+        document.getElementById("cell" + ( i * numRows + j )).style.background="pink";
+    } else {
+        document.getElementById("cell" + ( i * numRows + j )).style.background="#f8f8f8";
+    }
+}
